d68ba3dc7aa29dcf73ac36c43147eb787fbc193c
Akhilesh17-Tech <akhilesh.tech99@gmail.com>
2022-04-05 11:14:40 +0530

    object structuring and image from CDN

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,46 @@
 import React from "react";
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Mobile Phone",
+      price: 9999,
+      qty: 1,
+    };
+  }
   render() {
+    //   object de-structuring means i want this properties from this.state which is constructor
+    const { title, price, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} alt="Akhilesh"></img>
+          <img style={styles.image} alt="" />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 12 }}>Phone</div>
-          <div style={{ color: 12 }}>Rs 9999</div>
-          <div style={{ background: "red" }}>Qty: 1</div>
-          <div className="cart-item-actions">{/* Buttons */}</div>
+          {/* without object destructuring we have to write this.state.propertName again & again */}
+          {/* <div style={{ fontSize: 25 }}>{this.state.title}</div> */}
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"
+            />
+          </div>
         </div>
       </div>
     );
